f155ca89208ea5fcefdafc08f03a5e4450e4d4e8
PorkShoulder <145848629+PorkShoulder@users.noreply.github.com>
2024-10-27 23:22:01 +0900

    01. CEngine 생성

---
+++ Lhj/Lhj.cpp
@@ -0,0 +1,227 @@
+﻿// WinAPI.cpp : 애플리케이션에 대한 진입점을 정의합니다.
+//
+
+#include "framework.h"
+#include "Lhj.h"
+
+#include <vector>
+
+#define MAX_LOADSTRING 100
+
+#define MY_MACRO(Name) Name##Test
+
+
+// 전역 변수
+HINSTANCE   hInst = nullptr;
+
+
+// 함수 선언
+BOOL                InitInstance(HINSTANCE, int);
+LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
+
+// SAL : 주석 언어
+int APIENTRY wWinMain(HINSTANCE hInstance   // 프로세스 주소(ID)
+    , HINSTANCE hPrevInstance // 안쓰이는 인자
+    , LPWSTR lpCmdLine
+    , int   nCmdShow)
+{
+    WNDCLASSEXW wcex = {};
+
+    wcex.cbSize = sizeof(WNDCLASSEX);
+    wcex.style = CS_HREDRAW | CS_VREDRAW;
+    wcex.lpfnWndProc = &WndProc;
+    wcex.cbClsExtra = 0;
+    wcex.cbWndExtra = 0;
+    wcex.hInstance = hInstance;
+    wcex.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_LHJ));
+    wcex.hCursor = LoadCursor(nullptr, IDC_ARROW);
+    wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+    wcex.lpszMenuName = MAKEINTRESOURCEW(IDI_LHJ);
+    wcex.lpszClassName = L"Key";
+    wcex.hIconSm = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+
+    RegisterClassExW(&wcex);
+
+    // HWND 윈도우 ID 타입
+    // 커널 오브젝트 ( OS 차원에서 관리되는 객체 )
+    HWND hWnd = CreateWindowW(L"Key", L"MyGame", WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
+
+    if (!hWnd)
+    {
+        return FALSE;
+    }
+
+    // 윈도우 크기 설정, 위치 설정
+    SetWindowPos(hWnd, nullptr, 10, 10, 1280, 768, 0);
+    ShowWindow(hWnd, true);
+    UpdateWindow(hWnd);
+
+
+    // 단축키 테이블 
+    HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDI_LHJ));
+
+
+
+    // 메시지 변수
+    MSG msg = {};
+
+    // GetMessage
+    // - 메세지 큐에 있는 메세지를 받아온다.
+    // - 메세지가 큐에 없으면, 함수가 반환되지 않는다.
+    // - 꺼낸 메세지가 WM_QUIT 이면, false 를 반환, 그 외에는 True 반환    
+    SetTimer(hWnd, 0, 50, 0);
+
+    // 메세지가 없으면, 프로그램이 동작하지 않는 구조이다.
+    // 게임을 만들기에 적합하지 않음
+    // 강제로 일정시간마다 일정한 메세지를 발생시킨다. 
+    //  - Timer 를 이용해서 WM_TIMER 메세지를 일정시간마다 발생시킨다.
+    //  - 메세지큐에 메시지가 없어도 프로그램이 중단되어있지 않는 구조가 필요
+    // GetMessage -> PeekMessage
+
+    // GetMessage 가 반환되었다 == 메세지가 있었다.
+    // 반환값이 false == 메세지가 WM_QUIT 이다.
+    // 반환값이 true ==  메세지가 WM_QUIT 아니다. 
+
+    // PeekMessage 가 반환되었다 == 메세지가 있었을 수도 있고, 없었을 수도 있다.
+    // 반환값이 true == 메세지가 있었다.
+    // 반환값이 false == 메세지가 없었다.
+
+    while (true)
+    {
+        // 메세지큐에 메세지가 있다.
+        if (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE))
+        {
+            // 메세지 처리
+            if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+            {
+                TranslateMessage(&msg);
+                DispatchMessage(&msg);
+            }
+        }
+
+        // 메세지큐에 메세지가 없다.
+        else
+        {
+            // 게임 실행
+
+        }
+    }
+
+    KillTimer(hWnd, 0);
+
+    return (int)msg.wParam;
+}
+
+
+// 1. 전역변수
+float g_X = 500.f;
+float g_Y = 300.f;
+
+float g_Width = 100.f;
+float g_Height = 100.f;
+
+
+
+// 메세지를 처리하는 함수
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    switch (message)
+    {
+    case WM_COMMAND:
+    {
+        int wmId = LOWORD(wParam);
+        // 메뉴 선택을 구문 분석합니다:
+        switch (wmId)
+        {
+        case IDM_ABOUT:
+            DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
+            break;
+        case IDM_EXIT:
+            DestroyWindow(hWnd);
+            break;
+        default:
+            return DefWindowProc(hWnd, message, wParam, lParam);
+        }
+    }
+    break;
+    case WM_PAINT:
+    {
+        PAINTSTRUCT ps;
+        HDC hdc = BeginPaint(hWnd, &ps);
+
+        // 무효화 영역 ( 화면을 다시 갱신해야 하는 영역 )
+
+        // 사각형 그리기
+        Rectangle(hdc
+            , g_X - g_Width / 2.f
+            , g_Y - g_Height / 2.f
+            , g_X + g_Width / 2.f
+            , g_Y + g_Height / 2.f);
+
+
+        EndPaint(hWnd, &ps);
+    }
+    break;
+    case WM_MOUSEMOVE:
+    {
+        int a = 0;
+    }
+    break;
+
+    case WM_KEYDOWN:
+    {
+        switch (wParam)
+        {
+        case 'W':
+            g_Y -= 1.f;
+            InvalidateRect(hWnd, nullptr, true);
+            break;
+        case 'S':
+            g_Y += 1.f;
+            InvalidateRect(hWnd, nullptr, true);
+            break;
+        case 'A':
+            g_X -= 1.f;
+            InvalidateRect(hWnd, nullptr, true);
+            break;
+        case 'D':
+            g_X += 1.f;
+            InvalidateRect(hWnd, nullptr, true);
+            break;
+        }
+    }
+    case WM_TIMER:
+    {
+        int a = 0;
+    }
+    break;
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+    default:
+        return DefWindowProc(hWnd, message, wParam, lParam);
+    }
+    return 0;
+}
+
+// 정보 대화 상자의 메시지 처리기입니다.
+INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    UNREFERENCED_PARAMETER(lParam);
+    switch (message)
+    {
+    case WM_INITDIALOG:
+        return (INT_PTR)TRUE;
+
+    case WM_COMMAND:
+        if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
+        {
+            EndDialog(hDlg, LOWORD(wParam));
+            return (INT_PTR)TRUE;
+        }
+        break;
+    }
+    return (INT_PTR)FALSE;
+}
